ccf3ca1eb0dfb6365a859f105d39008ec11d3c2a
geomaka <107925640+geomaka@users.noreply.github.com>
2023-12-01 00:46:09 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,14 +10,7 @@ const app = express();
 app.use(express.json())
 app.use(cors());
 app.use('/books',bookRoutes)
-//middleware for handling cors policy
-// app.use(cors(
-//     // {
-//     //     origin : 'http://localhost:5173',
-//     //     methods : ['GET','POST','PUT','DELETE'],
-//     //     allowHeaders : ['content-type']
-//     // }
-// ));
+
 
 app.get('/',(req,res)=>{
     return res.status(234).send('welcome to mern stack')
